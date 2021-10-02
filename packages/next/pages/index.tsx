eb86db9db842cc47105e760a7d7c60cf275d8d29
Matteo Mazzarolo <me@mazzarolomatteo.com>
2021-10-02 14:56:37 +0200

    Update index.tsx

--- packages/next/pages/index.tsx
+++ packages/next/pages/index.tsx
@@ -1,4 +1,7 @@
+// Many libraries in the React ecosystem use the setImmediate() API (like react-native-reanimated), 
+// which Next.js doesn't polyfill by default.
 import 'setimmediate';
+
 import { App } from '@my-app/app';
 
 export default App;
